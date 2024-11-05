15675c29381ef265796cdf8d94d3db61471b6ce8
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-11-05 12:57:37 +0530

    Create CountDown.js

---
+++ 01-js/easy/CountDown.js
@@ -0,0 +1,19 @@
+function startCountdown(duration) {
+  let timer = duration;
+  const countdownElement = 0
+
+  const interval = setInterval(() => {
+    const seconds = timer % 60;
+
+    countdownElement.textContent = `Time left: ${seconds} seconds`;
+
+    if (timer === 0) {
+      clearInterval(interval);
+      countdownElement.textContent = "Time's up!";
+    } else {
+      timer--;
+    }
+  }, 1000);
+}
+
+startCountdown(30);
